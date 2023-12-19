24e5ba37198bb85e4fb68fddd4631d04b9590383
Tamires Santos <tfs.tamsantos@gmail.com>
2023-12-19 09:54:43 -0300

    Utilizando bibliotecas para dano aleatório

--- projeto/jogo.py
+++ projeto/jogo.py
@@ -1,6 +1,7 @@
 # Personagem: classe mae
 # Heroi: controlado pelo usuario
 # Inimigo: adversario do usuario
+import random
 
 class Personagem:
   def __init__(self, nome, vida, nivel):
@@ -26,7 +27,7 @@ class Personagem:
       self.__vida = 0
   
   def atacar(self, alvo):
-    dano = self.__nivel * 2
+    dano = random.randint(self.get_nivel() * 2, self.get_nivel() * 4) # baseado no nivel
     alvo.receber_ataque(dano)
     print(f"{self.get_nome()} atacou {alvo.get_nome()} e causou {dano} de dano!")
   
@@ -42,7 +43,7 @@ class Heroi(Personagem):
     return f"{super().exibir_detalhes()}\nHabilidade: {self.get_habilidade()}\n"
   
   def ataque_especial(self, alvo):
-    dano = self.get_nivel() * 5 # Dano aumentado
+    dano = random.randint(self.get_nivel() * 5, self.get_nivel() * 8) # Dano aumentado
     alvo.receber_ataque(dano)
     print(f"{self.get_nome()} usou a habilidade especial {self.get_habilidade()} em {alvo.get_nome()} e causou {dano} de dano!")
   
